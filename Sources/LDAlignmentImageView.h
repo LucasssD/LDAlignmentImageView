e9cde0c8f50ca029e940cd91cd19a0ee85fbcfb7
Aral Balkan <aral@ind.ie>
2020-01-08 11:09:27 +0000

    Fixes #2: workaround/fix for the iOS 13 image duplication issue
    Breaking change: must use correctImage property instead of image property.

--- Sources/LDAlignmentImageView.h
+++ Sources/LDAlignmentImageView.h
@@ -1,5 +1,16 @@
 //
 //  LDImageView.h
+//  AutoLayout
+//
+//  Created by Aral Balkan on 11/10/2018.
+//  Copyright © 2018 Aral Balkan. All rights reserved.
+//
+
+#ifndef LDAlignmentImageView_h
+#define LDAlignmentImageView_h
+
+//
+//  LDImageView.h
 //  FotoGis
 //
 //  Created by Lucas Duda on 31.12.2016.
@@ -32,5 +43,8 @@ typedef enum LDImageContentMode{
 @property (nonatomic) LDImageVerticalAlignment imageVerticalAlignment;
 @property (nonatomic) LDImageHorizontalAlignment imageHorizontalAlignment;
 @property (nonatomic) LDImageContentMode imageContentMode;
+@property (nonatomic) UIImage* correctImage;
 
 @end
+
+#endif /* LDAlignmentImageView_h */
